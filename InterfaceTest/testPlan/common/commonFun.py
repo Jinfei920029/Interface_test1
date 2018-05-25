2a8f375d024ac275170418dbb60e554229ba7903
Jinfei920029 <2213090222@qq.com>
2018-05-25 11:03:48 +0800

    code improve and update

--- InterfaceTest/testPlan/common/commonFun.py
+++ InterfaceTest/testPlan/common/commonFun.py
@@ -1,12 +1,24 @@
 import re
+import json
 
 class Fun:
+    #读取文件内容
+    def take_token(path):
+        with open(path, 'r') as load_f:
+            load_dict = json.load(load_f)
+            for value in load_dict.values():
+                print("take_token:" + value)
+                return value
     #定义函数jsonFile用来保存获取的response到指定文件
     def save_to_jsonfile(file_name, contents):
-        fh = open(file_name, 'w')
-        fh.write(contents)
-        fh.close()
-        print("Data saved ok")
+        try:
+            fh = open(file_name, 'w')
+            fh.write(contents)
+        except IOError:
+            print("Error: 没有找到文件或读取文件失败")
+        else:
+            print("内容写入文件成功")
+            fh.close()
     #定义函数getSession用来获取返回的sessionid和sessiondate
     def getSessionId(response):
         # 正则表达式截取返回结果的SessionID格式：res = r'<td>(.*?)</td><td>(.*?)</td>'
@@ -14,12 +26,35 @@ class Fun:
         m = re.findall(res, response, re.S | re.M)
         sessionid = str(m[0])
         return sessionid
+    def getSessionId1(response):
+        # 正则表达式截取返回结果的SessionID格式：res = r'<td>(.*?)</td><td>(.*?)</td>'
+        try:
+            res = r'<input type="hidden" name="sessionId" value="(.*?)"/>'
+            m = re.findall(res, response, re.S | re.M)
+            sessionid = str(m[0])
+        except Exception as e:
+            print(e)
+        else:
+            print("获取到SessionId")
+            return sessionid
+    #SessionData函数处理
     def getSessionData(response):
         # 正则表达式截取返回结果的SessionData格式：res = r'<td>(.*?)</td><td>(.*?)</td>'
         res = r'<input type="hidden" name="sessionData" value="(.*?)"/>'
         m = re.findall(res, response, re.S | re.M)
         sessiondata = str(m[0])
         return sessiondata
+    def getSessionData1(response):
+        # 正则表达式截取返回结果的SessionID格式：res = r'<td>(.*?)</td><td>(.*?)</td>'
+        try:
+            res = r'<input type="hidden" name="sessionData" value="(.*?)"/>'
+            m = re.findall(res, response, re.S | re.M)
+            sessiondata = str(m[0])
+        except Exception as e:
+            print(e)
+        else:
+            print("获取到SessionId")
+            return sessiondata
     #两个字典类型合并的函数
     def merge_Two_Dicts(x,y):
         z = x.copy()
@@ -43,6 +78,6 @@ class Login_Element:
                'ha_id': 'SIEMENS-KA92NP49TI-68A40E005BA3',
                'device_code': '123456789'}
     message2 = {"client_id": "970F224CEB3BC18966E2A0564D00B88235FCB2753FCC9C98C1208199EF651F07",
-                "email": "pcn80507@grr.la",
+                "email": "pcn80130@mailinator.com",
                 "password": "qwer-1234",
                 }
--- InterfaceTest/testPlan/login/test.py
+++
@@ -1,9 +0,0 @@
-from testPlan.common.commonFun import Fun
-#save_to_file('mobiles.txt', 'your contents str')
-response3 = "jinfei2"
-token_info = Fun.re_write(response3)
-
-Fun.save_to_jsonfile('../common/token.json', token_info)
-
-print(token_info)
-print(type(token_info))
