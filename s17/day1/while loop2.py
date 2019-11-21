921cff26e5939aa186145dbcccc89c480fe359d5
Jinfei920029 <2213090222@qq.com>
2019-11-21 17:10:53 +0800

    1

---
+++ s17/day1/while loop2.py
@@ -0,0 +1,21 @@
+# -*- coding:utf-8 -*-
+# Author:Jin Fei
+# while loop
+akon_age  = 28
+count = 0
+while count < 3:
+    age_input = int(input("The akon's age:"))
+    if age_input == akon_age:
+        print("yes ,you got it !")
+        break
+    elif age_input < akon_age:
+        print("too small")
+    else:
+        print("too old")
+    count += 1
+    if count == 3:
+        confirm = input("do you want to continue? y/n")
+        if confirm != "n":
+            count = 0
+        else:
+            break
