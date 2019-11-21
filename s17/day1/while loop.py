81b168acfd640d5fb471cc7897cf1aa523ecf9d2
Jinfei920029 <2213090222@qq.com>
2019-11-21 11:28:11 +0800

    while loop test

---
+++ s17/day1/while loop.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# Author:Jin Fei
+
+akon_age  = 28
+count = 0
+while count < 3:
+    age_input = int(input("The akon's age:"))
+    if age_input == akon_age:
+        print("yes ,you got it !")
+        break
+    elif age_input < akon_age:
+        print("too small")
+    else:
+        print("too old")
+    count += 1
+else:
+    print("you have tried too many time , fuck off")
